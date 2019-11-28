bb9b5b6dbdc86e84d673f53b901e66600ad8068a
taruc <tanys-wa16@studen.tarc.edu.my>
2019-11-28 16:14:34 +0800

    Initial

---
+++ app/src/main/java/com/example/databindingintent/SecondActivity.kt
@@ -0,0 +1,48 @@
+package com.example.databindingintent
+
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val name = intent.getStringExtra(MainActivity.EXTRA_NAME)
+        val phone = intent.getStringExtra(MainActivity.EXTRA_PHONE)
+        textViewreceiveName.text = name.toString()
+        textViewreceiveName.text = phone.toString()
+
+        buttonDone.setOnClickListener {
+            val intent = getIntent()
+            if(editTextReply.text.isEmpty())
+            {
+                val reply = editTextReply.text.toString()
+
+                intent.putExtra(MainActivity.EXTRA_REPLY,reply)
+                setResult(Activity.RESULT_OK,intent)
+            }
+            else
+            {
+                setResult(Activity.RESULT_CANCELED,intent)
+            }
+            finish()
+        }
+        buttonCall.setOnClickListener {
+            val phone = Uri.parse("tel" + textViewreceivePhone.text.toString())
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(phone)
+            if(intent.resolveActivity(packageManager)!=null)
+            {
+                startActivity(intent)
+            }
+
+        }
+    }
+}
